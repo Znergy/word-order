14756a8ab65542f41a2172d8ecd33329be2bc59b
Chris Carr and Ryan Jones <chris@>
2017-02-20 15:57:58 -0800

    add the logic to sort user input and output the number of times that word displays on the screen

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $("#wordForm").submit(function(event) {
+    event.preventDefault();
+
+    var userInput = $("#textInput").val();
+
+    var wordArray = userInput.split(" ");
+    var prev = "";
+    var a = [];
+    var b = [];
+
+
+    wordArray.sort();
+
+    wordArray.forEach(function(word) {
+
+      if(word !== prev) {
+        a.push(word);
+        b.push(1);
+      } else {
+        b[b.length - 1]++;
+      }
+
+      prev = word;
+
+    });
+
+    console.log(a);
+    console.log(b);
+
+    for (i=0; i<a.length; i++) {
+      $("#resultUL").append("<li>The word is " + a[i] + ", the count is " + b[i] + ".</li>");
+    }
+  });
+});
